1a3d5f19c91162ce1a952ba3603a02cef342bd3d
TrNghia16509 <101879831+TrNghia16509@users.noreply.github.com>
2024-08-13 20:02:58 +0700

    Add files via upload

---
+++ PS2_controller_and_Sensor.h
@@ -0,0 +1,198 @@
+//khai báo hàm cần dùng 
+#pragma once
+#include <PS2X_lib.h>
+#include "motors_and_servos.h"
+#include <Adafruit_Sensor.h>// Thư viện cảm biến
+#include <Adafruit_TCS34725.h>// Thư viện cảm biến màu sắc TCS34725
+#include <Wire.h>
+//#include <motors_and_servos.h>
+
+PS2X ps2x; // create PS2 Controller Class
+
+#define PS2_DAT 12 // MISO  19
+#define PS2_CMD 13 // MOSI  23
+#define PS2_SEL 15 // SS     5
+#define PS2_CLK 14 // SLK   18
+
+void setupPS2controller() // hàm kết nối tay cầm PS2 với mạch
+{
+  int err = -1;
+  while (err != 0){
+  err = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, true, true);
+  }
+}
+bool PS2control_loop() // hàm sử dụng tay cầm PS2 để điều khiển
+{
+  
+  ps2x.read_gamepad(false,false);
+
+  int nJoyLY = ps2x.Analog(PSS_LY); // đọc giá trị trục Oy của joystick bên trái
+  int nJoyRY = ps2x.Analog(PSS_RY); // đọc giá trị trục Oy của joystick bên phải
+
+  // chuyển đổi giá trị trục Oy của các joystick thành giá trị xung
+  // set map ngược để 2 bên động cơ có thể chạy theo ý muốn
+  nJoyLY = map(nJoyLY, 0, 256, -4092, 4092);
+  nJoyRY = map(nJoyRY, 0, 256, 4092, -4092);
+  // điểu khiển DC1 với giá trị trục Oy của joystick bên trái
+  if (nJoyLY>0)
+  {
+    DC1(nJoyLY, 0);
+    delay(50);
+  }
+  else if (nJoyLY<0)
+  {
+    DC1(0, nJoyLY);
+    delay(50);
+  }
+  else { DC1(0, 0);
+  delay(50);}
+
+ // điểu khiển DC2 với giá trị trục Oy của joystick bên phải
+  if (nJoyRY>0)
+  {
+    DC2(nJoyRY, 0);
+    delay(50);
+  }
+  else if (nJoyRY<0)
+  {
+    DC2(0, nJoyRY);
+    delay(50);
+  }
+  else { DC2(0, 0);
+  delay(50);}
+
+  // điểu khiển S360MO bằng nút hình tròn và nút sang trái
+  // hình tròn: sang phải; nút sang trái: sang trái
+  if (ps2x.Button(PSB_CIRCLE))
+  {
+    S360_MO(4096);
+    delay(50);
+  }
+  else if (ps2x.Button(PSB_PAD_LEFT))
+  {
+    S360_MO(0);
+    delay(50);
+  }
+  else {S360_MO(2048);}
+
+  // điểu khiển DC3 bằng RB và RT (ấn 1 lần sẽ quay và ấn lần nữa sẽ dừng)
+  // RB: DC3 quay theo chiều thuận; RT: DC3 quay theo chiều ngược
+  if (ps2x.NewButtonState(PSB_R2))
+  {
+    DC3(2048, 0);
+    delay(50);
+  }
+  else if (ps2x.NewButtonState(PSB_R2))
+  {
+    DC3(0, 0);
+    delay(50);
+  }
+
+  if (ps2x.NewButtonState(PSB_R1))
+  {
+    DC3(0 ,2048);
+    delay(50);
+  }
+  else if (ps2x.NewButtonState(PSB_R1))
+  { DC3(0, 0);
+  delay(50);
+  }
+
+//điểu khiển S180_OP bằng LB
+//nếu được nhấn thì mở cổng ngược lại thì đóng cổng
+  if (ps2x.ButtonPressed(PSB_L2))
+  {
+    S180_OP(1500);
+  }
+  else 
+  {
+    S180_OP(1000);
+  }
+
+ // điểu khiển DC4 bằng LT
+ // nếu giá trị của LT lớn hơn 2048 thì bắn bóng theo độ mạnh của lực nhấn vào LT ngược lại thì đứng yên
+  int nTrigL = ps2x.Analog(PSB_L1);
+  nTrigL = map(nTrigL, 0 ,255, 0, 4096);
+  if (nTrigL > 2048)
+  {
+    DC4(nTrigL);
+  }
+  else DC4(0);
+
+ // điểu khiển S360PU bằng nút hình tam giác 
+ // nếu nút tam giác được nhấn thì đẩy bóng ngược lại thả ra thì quay lại vị trí cũ
+  if (ps2x.ButtonPressed(PSB_TRIANGLE))
+  {
+    S360_PU(4095);
+    delay(1000);
+  }
+  else {S360_PU(0);
+  delay(1000);}
+  delay(100);
+  return 1;
+}
+
+ 
+ //khai báo các chân cảm biến
+#define SCL 22
+#define SDA 21
+/*
+  SCL - SCL ( Analog 5 )
+  SDA - SDA ( analog 4 )
+  VDD - 3.3V DC
+  GND - GND
+*/
+
+/* Sử dụng cảm với thời gian cụ thể và Gain cụ thể
+ * integration time (Thời gian lấy mẫu màu của cảm biến): 2.4ms, 24ms, 50ms, 101ms, 154ms, 600ms
+ * Gain (Độ lợi kiểm soát độ nhạy màu của biến): 1x, 4x, 16x, 60x
+ */
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_154MS, TCS34725_GAIN_16X);
+ 
+ // kết nối cảm biến với mạch
+void setupSensor() 
+{
+  Serial.begin(9600);
+ 
+  if (tcs.begin()) 
+  {
+    Serial.println("Cảm biến hoạt động");
+  } 
+  else 
+  {
+    Serial.println("Cảm biến không kết nối, hãy kiểm tra lại kết nối...");
+    while (1);
+  }
+  
+}
+ 
+ // hàm sử dụng cảm biến 
+void loopSensor() 
+{
+    uint16_t r, g, b, c, colorTemp, lux; //khai báo các biến cần dùng 
+     
+    tcs.getRawData(&r, &g, &b, &c);// lấy dữ liệu theo bảng màu RGB
+    
+    colorTemp = tcs.calculateColorTemperature(r, g, b); //Nhiệt độ màu theo thang đo Kelvin
+    
+    lux = tcs.calculateLux(r, g, b); //Độ rọi soi, cường độ sáng
+     
+    //điểu khiển S180DE theo màu sắc của bóng
+    //nếu bóng màu đen thì quay servo sao cho bóng đen qua bên màu đen
+    //nếu bóng màu trắng thì quay servo sao cho bóng trắng qua bên màu trắng
+    //nếu không có bóng thì quay về giữa
+    
+    S180_DE(1500);
+    if (r==255 && g==255 && b==255)
+    {
+      S180_DE(1389);
+    }
+    else if (r==0 && g==0 && b==0)
+    {
+      S180_DE(1772);
+    }
+    else {
+    S180_DE(1500);}
+    delay(50);
+}
---
+++ motors_and_servos.h
@@ -0,0 +1,97 @@
+// khai báo các thư viện cần sử dụng
+#pragma once
+#include <stdio.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>// Thư viện PCA9685
+#include <PS2X_lib.h>// Thư viện PS2
+
+// Khai báo các chân servo và động cơ 
+// PWM channels of pca9685 0-16
+#define DC1_chan1 8
+#define DC1_chan2 9
+#define DC2_chan1 10
+#define DC2_chan2 11
+#define DC3_chan1 12
+#define DC3_chan2 13
+#define DC4_chan1 14
+#define DC4_chan2 15
+#define S360MO_chan 7
+#define S360PU_chan 6
+#define S180DE_chan 5
+#define S180OP_chan 2
+//Khai báo clas với của thư viện với địa chỉ gốc
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x70);
+void setupMotors() // hàm kết nối động cơ với mạch
+{
+  Wire.begin(); // SDA, SCL,400000);
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(50);
+  Wire.setClock(400000);
+  // bắt đầu: cho các động cơ không chạy
+  pwm.setPWM(DC1_chan1, 0, 0);
+  pwm.setPWM(DC1_chan2, 0, 0);
+  pwm.setPWM(DC2_chan1, 0, 0);
+  pwm.setPWM(DC2_chan2, 0, 0);
+  pwm.setPWM(DC3_chan1, 0, 0);
+  pwm.setPWM(DC3_chan2, 0, 0);
+  pwm.setPWM(DC4_chan1, 0, 0);
+}
+
+void DC1(int LY_1, int LY_2) // hàm điểu khiển DC1 (động cơ di chuyển bên trái)
+{ 
+   pwm.setPWM(DC1_chan1, 0, LY_1);
+   pwm.setPWM(DC1_chan2, 0, LY_2);
+}
+
+void DC2(int RY_1, int RY_2) // hàm điểu khiển DC2 (động cơ di chuyển bên phải)
+{
+   pwm.setPWM(DC2_chan1, 0, RY_1);
+   pwm.setPWM(DC2_chan2, 0, RY_2);
+} 
+
+void DC3(int R_1, int R_2) // hàm điểu khiển DC3 (động cơ intake)
+{ 
+   pwm.setPWM(12, 0, R_1);
+   pwm.setPWM(13, 0, R_2);
+    delay(50);
+}
+
+void DC4(int quay) // hàm điểu khiển DC4 (động cơ bắn bóng)
+{
+    pwm.setPWM(14, 0, quay);
+    pwm.setPWM(15, 0, 0);
+}
+
+void setupServo() // hàm kết nối servo với mạch
+{
+  Wire.begin(); // SDA, SCL,400000);
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(50);
+  Wire.setClock(400000);
+}
+
+void S360_MO(int move) // hàm điểu khiển S360MO (servo di chuyển ngang)
+{
+    pwm.setPWM(S360MO_chan, 0, move);
+    delay(50);
+}
+
+void S180_OP(int open) // hàm điểu khiển S180OP (servo mở/đóng cổng thoát chất thải)
+{
+    pwm.writeMicroseconds(S180OP_chan, open);
+    delay(50);
+}
+  
+void S180_DE( int deviced) // hàm điều khiển S180DE  (servo chia bóng)
+{
+    pwm.writeMicroseconds(S180DE_chan, deviced);
+    delay(50);
+}
+
+void S360_PU( float push) // hàm điểu khiển S360PU (servo đẩy bóng)
+{
+    pwm.setPWM(S360PU_chan, 0, push);
+    delay(50);
+}
